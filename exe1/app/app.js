9db0c5d30ac7f389264ceb098fe733d4bdb56169
alexandredejesus <c.aledesigner@gmail.com>
2023-04-26 18:21:36 -0300

    .

---
+++ exe1/app/app.js
@@ -0,0 +1,13 @@
+import { LightningElement } from 'lwc';
+
+export default class App extends LightningElement {
+
+    primeironome = 'Joaozinho';
+    sobrenome = 'Trinta';
+    idade = 85;
+    email = 'joaozinho@gmail.com';
+    classificacao = 'MQL';
+    empresa = 'Escola de Samba';
+    receitaanual = 'R$1,000,000';
+}
+
